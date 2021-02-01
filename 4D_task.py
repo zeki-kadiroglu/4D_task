0372e1fa4fd89ac878810c634f81cf62bee6ea3d
Zeki Kadiroğlu <z.kdrglu07@gmail.com>
2021-02-01 05:30:16 +0300

    modified

--- 4D_task.py
+++ 4D_task.py
@@ -15,23 +15,24 @@ def find_image(img, template):
   w, h = template.shape[::-1]
   img = img2.copy()
   method = eval('cv2.TM_SQDIFF')
-  #image matching
   res = cv2.matchTemplate(img,template,method)
-  #our values
-  min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-  #left side position
-  top_left = min_loc
-  #right side position
-  bottom_right = (top_left[0] + w, top_left[1] + h)
-  cv2.rectangle(img,top_left, bottom_right, 255, 2)
-  #illustration  	
-  plt.subplot(121),plt.imshow(res,cmap = 'gray')
-  plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-  plt.subplot(122),plt.imshow(img,cmap = 'gray')
-  plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-  plt.suptitle('Find Image') 
-  plt.show()
-  return f'coordinates : {min_loc}, {max_loc}'
+  if res.any():
+    #our values
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    #left side position
+    top_left = min_loc
+    #right side position
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+    cv2.rectangle(img,top_left, bottom_right, 255, 2)
+     #illustration
+    plt.subplot(121),plt.imshow(res,cmap = 'gray')
+    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(img,cmap = 'gray')
+    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+    plt.suptitle('Find Image') 
+    plt.show()
+    return f'coordinates : {min_loc}, {max_loc}'
+  return 'There is no matching image'
   
 
 #For rotated image
